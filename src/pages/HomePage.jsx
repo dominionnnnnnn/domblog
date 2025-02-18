6c4957578bae90712306d793c1df594b8d26289e
dominion <adebiyiquawiy2006@gmail.com>
2025-02-18 21:34:41 +0100

    Deploy latest version

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Body from '../components/Body'
+
+const HomePage = () => {
+   
+      
+  return (
+    <>
+        <Header/>
+        <Body/>
+    </>
+  )
+}
+
+export default HomePage
